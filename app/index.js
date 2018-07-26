363fd1bb63fc058528fdb9477cd7b8b7586d5abe
Sean Prouty <prouty_411@hotmail.com>
2018-07-26 15:52:03 -0700

    completed class demo

--- app/index.js
+++ app/index.js
@@ -1,1 +1,15 @@
-console.log('App working: open app/index.js to start exploring ES6');
+// import elementToBody from './add_to_dom';
+// import $ from 'jquery';
+import {Person,SuperPerson} from './person';
+const bob = new Person('cartman');
+console.log(bob.speak('my name is cartman'));
+const kermit = new SuperPerson('kermit' , 'instant baked potatoes');
+kermit.speak('Hello , I am kermit the frog');
+kermit.saySuperpower();
+
+
+// elementToBody('h1','this stuff is insane!!');
+// elementToBody('p','here is some other stuff ,battle of mount fuji!!');
+// $('body').append('<h1> i came from jquery sucka </h1>');
+
+
